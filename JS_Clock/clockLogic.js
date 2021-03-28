d8638626980d0ea9e8a33491562b4197bed5da66
Quentin McMillian <quentinmcmillian@gmail.com>
2021-03-28 15:47:48 -0400

    Minute hand now funcitons

--- JS_Clock/clockLogic.js
+++ JS_Clock/clockLogic.js
@@ -1,12 +1,19 @@
 
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand =  document.querySelector('.hour-hand');
 
 const setDate = () => {
   const now = new Date();
+  // Seconds Logic
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds/ 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds)
+
+  // Minutes Logic
+  const minutes =  now.getMinutes();
+  const minuteDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
