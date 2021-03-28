75d72a6ea48e1780d853dcdd086e85b221fb417c
Quentin McMillian <quentinmcmillian@gmail.com>
2021-03-28 15:37:08 -0400

    Seconds hand now functions

--- JS_Clock/clockLogic.js
+++ JS_Clock/clockLogic.js
@@ -1,1 +1,12 @@
-console.log('Clock logic working');
+
+const secondHand = document.querySelector('.second-hand');
+
+const setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds/ 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds)
+}
+
+setInterval(setDate, 1000);
