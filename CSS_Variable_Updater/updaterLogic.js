d8779c5d620d21226abec138f1f6bcea1d56092e
Quentin McMillian <quentinmcmillian@gmail.com>
2021-03-30 01:22:35 -0400

    CSS controls now function correctly

--- CSS_Variable_Updater/updaterLogic.js
+++ CSS_Variable_Updater/updaterLogic.js
@@ -1,1 +1,11 @@
-console.log('testing 1 2')
+// makes node list of all inputs within the conrol class
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || ''; // accessed by adding a data attribute to the input html element  || '' is a fallback
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+// listens to any change events on the inputs and calls handleUpdate
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
